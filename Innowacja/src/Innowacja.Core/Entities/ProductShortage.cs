4145a21cafe2d0d93e9aeb974a67689650058c27
Jakub Malkowski <malkowski2003@gmail.com>
2025-02-09 21:24:30 +0100

    add description productShortage

--- Innowacja/src/Innowacja.Core/Entities/ProductShortage.cs
+++ Innowacja/src/Innowacja.Core/Entities/ProductShortage.cs
@@ -3,16 +3,16 @@ namespace Innowacja.Core.Entities
 {
     public class ProductShortage
     {
-        public int ShortageId { get; set; }
-        public int shopShelfId { get; set; }
-        public string ProductName { get; set; } = null!;
-        public int ProductNumber { get; set; }
-        public int ShelfNumber { get; set; }
+        public int ShortageId { get; set; } //id braku produktu
+        public int shopShelfId { get; set; } //id regalu
+        public string ProductName { get; set; } = null!; //nazwa produktu
+        public int ProductNumber { get; set; } //numer produktu na półce od lewej strony
+        public int ShelfNumber { get; set; } // numer półki od góry danego regału
         public double Xmin { get; set; }
         public double Xmax { get; set; }
         public double Ymin { get; set; }
         public double Ymax { get; set; }
-        public string FilePath { get; set; }
+        public string FilePath { get; set; }  //sciezka do pliku
 
         public Shelf Shelf { get; set; } = null!;
     }
